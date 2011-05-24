552265d77547e36dd6fc6208b323e91847c54647
Pippijn van Steenhoven <pip88nl@gmail.com>
2011-05-25 00:02:08 +0200

    moved file; added OWL schema

--- src/validate.h
+++ src/validate.h
@@ -4,20 +4,21 @@ validate (std::string const &input)
   timer const T (__func__);
   XercesDOMParser *parser = new XercesDOMParser;
   parser->setExternalSchemaLocation (
-    "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul "SRCDIR"/xsd/xul.xsd "
-    "http://www.w3.org/1999/xhtml "SRCDIR"/xsd/xul-html.xsd "
-    "http://www.w3.org/2001/XInclude "SRCDIR"/xsd/xml/xinclude.xsd "
-    "http://www.w3.org/2001/SMIL20/ "SRCDIR"/xsd/smil/smil20.xsd "
-    "http://www.w3.org/XML/1998/namespace "SRCDIR"/xsd/xml/namespace.xsd "
-    "http://www.w3.org/2000/10/xlink-ns "SRCDIR"/xsd/xml/xlink.xsd "
+    "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul      "SRCDIR"/xsd/xul.xsd            "
+    "http://www.w3.org/1999/xhtml                                       "SRCDIR"/xsd/xul-html.xsd       "
+    "http://www.w3.org/2000/10/xlink-ns                                 "SRCDIR"/xsd/xml/xlink.xsd      "
+    "http://www.w3.org/2001/SMIL20/                                     "SRCDIR"/xsd/smil/smil20.xsd    "
+    "http://www.w3.org/2001/XInclude                                    "SRCDIR"/xsd/xml/xinclude.xsd   "
+    "http://www.w3.org/2002/07/owl#                                     "SRCDIR"/xsd/owl/owl.xsd        "
+    "http://www.w3.org/XML/1998/namespace                               "SRCDIR"/xsd/xml/xml.xsd        "
   );
-  parser->setValidationScheme (XercesDOMParser::Val_Always);
+  parser->setCreateEntityReferenceNodes (true);
   parser->setDoNamespaces (true);
   parser->setDoSchema (true);
+  parser->setDoXInclude (true);
   parser->setHandleMultipleImports (true);
   parser->setValidationSchemaFullChecking (true);
-  parser->setCreateEntityReferenceNodes (true);
-  parser->setDoXInclude (true);
+  parser->setValidationScheme (XercesDOMParser::Val_Always);
 
   DOMTreeErrorReporter handler;
   parser->setErrorHandler (&handler);
